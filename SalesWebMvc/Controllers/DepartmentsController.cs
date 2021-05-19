d7e3f302746b0990fcee5f9ca34574b8ada66d62
Rodrigo <rodrigo.muniz.dev@gmail.com=>
2021-05-18 21:14:12 -0300

    Contruida seção Departments

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> Departments = new List<Department>();
+            Departments.Add(new Department { Id = 1, Name = "Garagem" });
+            Departments.Add(new Department { Id = 2, Name = "Elétrica" });
+            Departments.Add(new Department { Id = 3, Name = "Oficina" });
+
+            return View(Departments);
+        }
+    }
+}
